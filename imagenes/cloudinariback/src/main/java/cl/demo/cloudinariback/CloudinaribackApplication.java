a948e88ea9fda4fe211450feca2dc41e2e34cb2b
usar77 <agenda212013@gmail.com>
2022-01-27 12:52:46 -0300

    Imagenes
    proyecto de imagenes cloudinary

---
+++ imagenes/cloudinariback/src/main/java/cl/demo/cloudinariback/CloudinaribackApplication.java
@@ -0,0 +1,13 @@
+package cl.demo.cloudinariback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudinaribackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudinaribackApplication.class, args);
+	}
+
+}
---
+++ imagenes/cloudinariback/src/test/java/cl/demo/cloudinariback/CloudinaribackApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.demo.cloudinariback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudinaribackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
