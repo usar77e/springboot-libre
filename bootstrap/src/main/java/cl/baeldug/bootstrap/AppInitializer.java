f5c30f8993c1874003a37de60c1df63b508a82bb
usar77 <agenda212013@gmail.com>
2022-02-14 15:29:25 -0300

    termino spring boat

---
+++ bootstrap/src/main/java/cl/baeldug/bootstrap/AppInitializer.java
@@ -0,0 +1,23 @@
+package cl.baeldug.bootstrap;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+public class AppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext container){
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.scan("com.baeldung");
+        container.addListener(new ContextLoaderListener(context));
+
+        ServletRegistration.Dynamic dispatcher = container.addServlet("mvc", new DispatcherServlet(context));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
+}
---
+++ bootstrap/src/main/java/cl/baeldug/bootstrap/WebConfig.java
@@ -0,0 +1,11 @@
+package cl.baeldug.bootstrap;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.baeldung.controller")
+public class WebConfig {
+}
